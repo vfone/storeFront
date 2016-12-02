571daf2ca8b84e59744b1fd8e8e6187bbbb450d0
Tang, Xiao <Xiao.Tang@news.com.au>
2016-12-02 11:57:30 +1100

    initial commit: yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name storeFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the storeFrontApp
+ */
+angular.module('storeFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name storeFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the storeFrontApp
+ */
+angular.module('storeFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
